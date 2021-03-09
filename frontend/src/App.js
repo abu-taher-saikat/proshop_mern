a0efc0115ac0ca87fc142b2d695d1774b2904af6
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-10 01:10:42 +0600

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+
 const App = () => {
   return (
     <>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <HomeScreen></HomeScreen>
         </Container>
       </main>
       <Footer></Footer>
