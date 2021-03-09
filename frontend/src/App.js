5cedb61bac58bca6ec2b2aba4bac470421aa24f4
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-10 00:32:06 +0600

    bootstrap installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to proshop</h1>
-    </div>
+    <>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './bootstrap.min.css';
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
