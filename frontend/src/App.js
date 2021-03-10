fe926ed9f68cf9410900fd0b030d59918b7a0bb0
saikat1095 <mdabutahersaikat@gmail.com>
2021-03-10 11:49:11 +0600

    front end ended

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,21 @@ import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router , Route} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <HomeScreen></HomeScreen>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} exact />
         </Container>
       </main>
       <Footer></Footer>
-    </>
+    </Router>
   );
 }
 
