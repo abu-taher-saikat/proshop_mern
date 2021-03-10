13ad0b49c6b8669390a44a1fa12e80893cd63934
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-10 23:36:05 +0600

    done

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const productRouter = require('./routes/productRouter');
 const colors = require('colors');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 
 
 
 
 
-dotenv.config({path : './config/config.env'});
+dotenv.config();
 const PORT = process.env.PORT || 5000
 const MODE = process.env.NODE_ENV
 
@@ -24,6 +25,12 @@ app.get('/',(req,res)=> {
 })
 
 app.use('/api/products', productRouter);
+
+
+// Error handler middlewares . 
+app.use(notFound);
+app.use(errorHandler);
+
 
 
 app.listen(PORT , console.log(`Server running in ${MODE} on port ${PORT}`.yellow.bold));
