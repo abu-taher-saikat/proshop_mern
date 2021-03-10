4594ae768472ce421b49d4cd21b3cdf080f0e7f3
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-11 00:38:26 +0600

    reducer and action added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducer';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList : productListReducer,
+});
 
 const initialState = {};
 
