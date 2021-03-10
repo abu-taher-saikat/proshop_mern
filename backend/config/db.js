8e5cd599975a6766e74568d995e30ef5aa3356d5
saikat1095 <mdabutahersaikat@gmail.com>
2021-03-10 13:50:10 +0600

    colors added

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB =  async ()=>{
+
+    try{
+        const conn = await mongoose.connect("mongodb+srv://saikat:saikat1095@cluster0.htwdq.mongodb.net/proShop",{
+            //must add in order to not get any error masseges:
+            useUnifiedTopology:true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`mongo database is connected!!! ${conn.connection.host} `.cyan.underline)
+    }catch(error){
+        console.error(`Error: ${error} `.red.underline.bold);
+        process.exit(1) //passing 1 - will exit the proccess with error
+    }
+
+}
+
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+import dotenv from 'dotenv';
 import express from 'express'
 import products from './data/products.js';
-import dotenv from 'dotenv'
+import connectDB from './config/db.js';
+import colors from 'colors';
 
 
-dotenv.config();
+dotenv.config({path : './config/config.env'});
+const PORT = process.env.PORT || 5000
+const MODE = process.env.NODE_ENV
+
 
 const app = express();
+
+connectDB();
+
+app.use(express.json());
 
 app.get('/',(req,res)=> {
     res.status('Api is running....')
@@ -22,6 +31,4 @@ app.get('/api/products/:id',(req,res)=>{
     res.json(product);
 })
 
-const PORT = process.env.PORT || 5000;
-const MODE = process.env.NODE_ENV;
-app.listen(PORT , console.log(`Server running in ${MODE} on port ${PORT}`));
+app.listen(PORT , console.log(`Server running in ${MODE} on port ${PORT}`.yellow.bold));
