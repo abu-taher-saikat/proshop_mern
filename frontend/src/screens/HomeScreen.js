cb121e0495d28fa2afff83290fd5b447c70b001b
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-11 11:26:42 +0600

    redux basic done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,32 @@
-import React,{useState, useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 import Product from '../components/Product';
-import axios from 'axios';
-// import products from '../products';
+
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(()=>{
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products')
-            setProducts(data);
-        }
-        fetchProducts()
-    },[])
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const {loading, error, products} = productList;
+
+
+    useEffect(()=>{ 
+        dispatch(listProducts())
+    },[dispatch])
 
 
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
+            {loading ? <h2>Loading</h2> : error ? <h3>{error}</h3> : <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product}></Product>
                     </Col>
                 ))}
-            </Row>
+            </Row>}
+
         </>
     )
 }
