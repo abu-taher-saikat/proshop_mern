b3b6fa0760a07941dfd6a596acd7eeb695c67193
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-20 22:49:50 +0600

    done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,23 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducer';
 import { productDetailsReducer, productListReducer } from './reducers/productReducer';
 
 const reducer = combineReducers({
     productList : productListReducer,
-    productDetails : productDetailsReducer
+    productDetails : productDetailsReducer,
+    cart : cartReducer
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')):  [];
+
+
+const initialState = {
+    cart : { 
+        cartItems : cartItemsFromStorage
+    }
+};
 
 const middleware = [thunk];
 
