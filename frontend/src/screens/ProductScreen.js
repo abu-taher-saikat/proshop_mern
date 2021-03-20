5d90456becf57274958e9c965d282f80e94fbd84
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-20 23:42:26 +0600

    cart screen add to functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -12,7 +12,7 @@ import Rating from '../components/Rating';
 
 
 const ProductScreen = ({history, match}) => {
-    const [qty, setQty] = useState(0);
+    const [qty, setQty] = useState(1);
     const dispatch = useDispatch();
     const productDetails = useSelector(state => state.productDetails);
     const {loading, error, product} = productDetails;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,11 @@ const reducer = combineReducers({
     cart : cartReducer
 });
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')):  [];
+
+
+const cartItemsFromStorage = localStorage.getItem('cartItem')
+  ? JSON.parse(localStorage.getItem('cartItem'))
+  : []
 
 
 const initialState = {
