409eda59cc7b379e5d8ebfd18150cde9cb97e046
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-21 10:30:38 +0600

    fix the router

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,30 +1,13 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
-const Product = require('../models/productModel');
 
 const router = express.Router();
+const {getProducts, getProductById} = require('../controllers/productController');
 
-// @desc Fetch all products
-// @route GET /api/products/
-// @access Public.
-router.get('/', asyncHandler(async (req,res)=>{
-    const products = await Product.find({});
-    res.json(products);
-}));
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 
 
-// @desc Fetch a single product
-// @route GET /api/products/:id
-// @access Public.
-router.get('/:id', asyncHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id);
-    if(product){
-        res.json(product);
-    }else{
-        res.status(404)
-        throw new Error('Product not found');
-    } 
-}))
 
 module.exports = router;
