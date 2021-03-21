d899cd338ccd0b830fdc3d6b3a985e30bab0cee2
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-21 11:42:38 +0600

    user auth done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+const { reset } = require('colors');
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
@@ -26,4 +27,58 @@ const authUser = asyncHandler(async (req,res) => {
     }
 })
 
-module.exports = {authUser}
+// @desc Register a new user
+// @route POST /api/users
+// @access Public.
+const registerUser = asyncHandler(async (req,res) => {
+    const {name, email, password} = req.body;
+    
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            isAdmin : user.isAdmin,
+            token : generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+
+// @desc Get User profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        res.json({
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            isAdmin : user.isAdmin,
+        })
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+
+module.exports = {authUser, getUserProfile, registerUser};
