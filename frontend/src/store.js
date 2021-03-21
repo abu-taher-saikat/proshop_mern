07601029b994842a49b0997a2c39ca5e02c2a4c8
saikat1095 <mdabuthaersaikat@gmail.com>
2021-03-21 15:59:08 +0600

    user login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,13 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducer';
 import { productDetailsReducer, productListReducer } from './reducers/productReducer';
+import { userLoginReducer } from './reducers/userReducer';
 
 const reducer = combineReducers({
     productList : productListReducer,
     productDetails : productDetailsReducer,
-    cart : cartReducer
+    cart : cartReducer,
+    userLogin : userLoginReducer
 });
 
 
@@ -16,10 +18,16 @@ const cartItemsFromStorage = localStorage.getItem('cartItem')
   ? JSON.parse(localStorage.getItem('cartItem'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+? JSON.parse(localStorage.getItem('userInfo'))
+: null
 
 const initialState = {
     cart : { 
         cartItems : cartItemsFromStorage
+    },
+    userLogin : {
+        userInfo : userInfoFromStorage
     }
 };
 
