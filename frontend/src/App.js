40188f31eb75a3d8fc2046f47a1b187e5ee0f201
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-08 23:34:24 +0600

    check out steups component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
