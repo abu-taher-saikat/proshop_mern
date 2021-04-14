ab2da58736e939addc3df60b0e13a057bab48cb1
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-14 15:05:54 +0600

    Payment checkout is done with pay pal

--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -1,26 +1,29 @@
 import {
-    ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST,
-    ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
 
 
 
-    ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST,
-    ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
 
 
-    ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_FAIL,
 
 
-    ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_REQUEST,
 
 
-    ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_RESET,
 
 
-    ORDER_PAY_FAIL, ORDER_PAY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
 
 
-    ORDER_PAY_RESET, ORDER_PAY_SUCCESS
+  ORDER_PAY_FAIL, ORDER_PAY_REQUEST,
+
+
+  ORDER_PAY_RESET, ORDER_PAY_SUCCESS
 } from '../constants/orderConstants'
   
   export const orderCreateReducer = (state = {}, action) => {
@@ -112,6 +115,10 @@ import {
         return {
           loading: false,
           error: action.payload,
+        }
+      case ORDER_LIST_MY_RESET :
+        return{ 
+          orders : []
         }
       default:
         return state
