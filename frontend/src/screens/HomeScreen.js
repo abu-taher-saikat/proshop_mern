9caffa376de47193b5d387fc735031f8a8896279
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-17 16:46:39 +0600

    product search option added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,15 +7,17 @@ import Message from '../components/Message';
 import Product from '../components/Product';
 
 
-const HomeScreen = () => {
+const HomeScreen = ({match}) => {
+    const keyword = match.params.keyword
+
     const dispatch = useDispatch();
     const productList = useSelector((state) => state.productList);
     const {loading, error, products} = productList;
 
 
     useEffect(()=>{ 
-        dispatch(listProducts())
-    },[dispatch])
+        dispatch(listProducts(keyword))
+    },[dispatch, keyword])
 
 
     return (
